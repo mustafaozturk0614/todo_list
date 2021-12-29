72f8d3209bc4ef5c974b4eb69ce0fd3c199d816a
musty1406 <musty1406@gmail.com>
2021-12-29 03:50:21 +0300

    yapılan to_doları listeleme

--- todo.js
+++ todo.js
@@ -1,13 +1,21 @@
 const form = document.getElementById("todo-form");
 const todoInput = document.getElementById("todo");
-const todoList = document.querySelector(".list-group");
+const todoList = document.querySelector("#td");
+const todolist2 = document.querySelector("#yt");
+const todolist3 = document.querySelector("#ot");
 const firsCardBody = document.querySelectorAll(".card-body")[0];
 const secondCardBody = document.querySelectorAll(".card-body")[1];
-const filter = document.querySelector("#filter");
+const filter = document.getElementById("filter");
+const filter1 = document.getElementById("filter1");
+const filter2 = document.getElementById("filter2");
 const clearButton = document.querySelector("#clear-todos");
+const date = document.querySelector("#date");
+
 $(document).ready(function() {
 
+
     eventListeners();
+
 });
 
 
@@ -18,11 +26,13 @@ function eventListeners() { // tüm event listenerler
 
     form.addEventListener("submit", addTodo);
     document.addEventListener("DOMContentLoaded", loadAllTodosUI());
+    document.addEventListener("DOMContentLoaded", loadAllOldTodosUI());
+    document.addEventListener("DOMContentLoaded", loadAllDOTodosUI());
     secondCardBody.addEventListener("click", deleteTodo);
     filter.addEventListener("keyup", filterTodos);
-    clearButton.addEventListener("click", clearAllTodos)
-
-
+    filter1.addEventListener("keyup", filterTodos1);
+    filter2.addEventListener("keyup", filterTodos2);
+    clearButton.addEventListener("click", clearAllTodos);
 }
 
 function clearAllTodos() {
@@ -41,10 +51,68 @@ function clearAllTodos() {
 
 }
 
-function filterTodos(e) {
+function transferTodos() {
+
+    console.log("naber");
+
+
+}
+
+function filterTodos(e, id) {
     const filterVAlue = e.target.value.toLowerCase();
 
-    const listItems = document.querySelectorAll(".list-group-item");
+    const listItems = document.querySelectorAll("#td");
+    console.log(listItems);
+
+    listItems.forEach(function(listItem) {
+        const text = listItem.textContent.toLowerCase();
+
+        if (text.indexOf(filterVAlue) === -1) {
+
+            listItem.setAttribute("style", "display: none !important")
+        } else {
+            listItem.setAttribute("style", "display: block")
+
+        }
+
+
+    })
+
+
+
+
+}
+
+function filterTodos1(e) {
+    const filterVAlue = e.target.value.toLowerCase();
+
+    const listItems = document.querySelectorAll("#yt");
+    console.log(listItems);
+
+    listItems.forEach(function(listItem) {
+        const text = listItem.textContent.toLowerCase();
+
+        if (text.indexOf(filterVAlue) === -1) {
+
+            listItem.setAttribute("style", "display: none !important")
+        } else {
+            listItem.setAttribute("style", "display: block")
+
+        }
+
+
+    })
+
+
+
+
+}
+
+function filterTodos2(e) {
+    const filterVAlue = e.target.value.toLowerCase();
+
+    const listItems = document.querySelectorAll("#ot");
+    console.log(listItems);
 
     listItems.forEach(function(listItem) {
         const text = listItem.textContent.toLowerCase();
@@ -66,23 +134,57 @@ function filterTodos(e) {
 }
 
 function deleteTodo(e) {
+    let name = document.querySelectorAll(".form-check-input");
+    console.log(name);
+    let check = e.target.id;
+
 
     if (e.target.className === 'fa fa-remove') {
 
-
         e.target.parentElement.parentElement.remove();
-        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent)
 
+
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
         showAlert("success", "Todo  başarıyla silindi");
     }
+    if (e.target.className === "form-check-input") {
+
+        if (document.querySelector("#" + check).checked == true) {
+            console.log(check);
+            let li = e.target.parentElement.parentElement.parentElement;
+            let date = e.target.parentElement.parentElement.parentElement.firstElementChild.textContent;
+            let todos = e.target.parentElement.parentElement.firstElementChild.textContent;
+            let item = addTodoToUI(todos, date);
+            li.remove();
+            addList(todolist2, item);
+
+            console.log(e.target.parentElement.parentElement.parentElement.textContent);
+            showAlert("success", "Todo  başarıyla aktarıldı");
+            addTodoDOToStorage(todos, date);
+            deleteTodoFromStorage(e.target.parentElement.parentElement.parentElement.textContent);
+
+        }
+
+
+    }
+
+
+
+
+
+
+
 
 }
 
-function deleteTodoFromStorage(deletetodo) {
+function deleteTodoFromStorage(deletedata) {
+    console.log(deletedata);
 
     let todos = getTodosFromStorage();
+
+
     todos.forEach(function(todo, index) {
-        if (todo === deletetodo) {
+        if (todo[0] + todo[1] === deletedata) {
             todos.splice(index, 1);
 
         }
@@ -91,30 +193,87 @@ function deleteTodoFromStorage(deletetodo) {
     });
 
     localStorage.setItem("todos", JSON.stringify(todos));
+    controlTime();
+}
+
+function controlTime() {
+    let todos = getTodosFromStorage();
+    var now = new Date();
+
+
+
+    todos.forEach(function(todo, index) {
+        let tm = new Date(todo[1]);
+
+        if (tm < now) {
+            todos.splice(index, 1);
+
+
+            let item = addTodoToUI(todo[0], todo[1]);
+            addList(todolist3, item);
+        }
+
+
+    });
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
 
 function loadAllTodosUI() {
     let todos = getTodosFromStorage();
     todos.forEach(function(todo) {
-        addTodoToUI(todo);
+        let item = addTodoToUI(todo[0], todo[1]);
+        addList(todoList, item);
+
+
     });
 
 
 
 }
 
+function loadAllOldTodosUI() {
+    let todos = getdontTodosFromStorage();
+    todos.forEach(function(todo) {
+        let item = addTodoToUI(todo[0], todo[1]);
+        addList(todolist3, item);
+
+
+    });
+
+
+}
+
+function loadAllDOTodosUI() {
+    let todos = getDoTodosFromStorage();
+    todos.forEach(function(todo) {
+        let item = addTodoToUI(todo[0], todo[1]);
+        addList(todolist2, item);
+
+
+    });
+}
+
 function addTodo(e) {
     /* <div class="alert alert-danger" role="alert">
     A simple dark alert—check it out!
     </div>*/
-
+    let todolists = todoList;
     const newTodo = todoInput.value.trim();
-    console.log(newTodo);
+    const newDate = date.value;
+    console.log(newTodo, newDate);
     if (newTodo === "") {
-        showAlert("danger", "lütfen bir todo girin")
+        showAlert("danger", "lütfen bir todo girin");
+    } else if (newDate === "") {
+        showAlert("danger", "lütfen bir tarih girin");
     } else {
-        addTodoToUI(newTodo);
-        addTodoToStorage(newTodo);
+
+        let item = addTodoToUI(newTodo, newDate);
+
+        addList(todolists, item);
+        let stor = [newTodo, newDate];
+
+        addTodoToStorage(newTodo, newDate);
+        addTodoAllToStorage(newTodo, newDate);
         showAlert("success", "todo başarıyla eklendi")
     }
 
@@ -122,21 +281,54 @@ function addTodo(e) {
     e.preventDefault();
 }
 
-function addTodoToUI(newTodo) {
+function addTodoToUI(newTodo, newDate) {
 
-    const listItem = document.createElement("li");
+    let listItem = document.createElement("li");
     const link = document.createElement("a");
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    const input = document.createElement("input");
+    const p = document.createElement("p");
+    const p2 = document.createElement("p");
+    input.className = "form-check-input";
+    input.type = "checkbox";
+    let list = getAllTodosFromStorage();
+
+    let number = list.length;
+    console.log(number);
+    input.id = "check" + number;
+    input.name = "check";
+    input.value = "checkedValue";
+    p.appendChild(document.createTextNode(newDate));
+
+
+    label.className = "form-check-label";
+
+    div.className = "form-check";
+    label.appendChild(input);
+    label.appendChild(p);
+    div.appendChild(label);
+
     link.href = "#";
     link.className = "delete-item";
     link.innerHTML = "<i class = 'fa fa-remove'></i>";
     listItem.className = "list-group-item d-flex justify-content-between";
-    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(p2).appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(div);
     listItem.appendChild(link);
 
-    todoList.appendChild(listItem);
+
     todoInput.value = "";
+    return listItem;
 
 
+
+
+}
+
+function addList(todolist, items) {
+
+    todolist.appendChild(items);
 
 
 }
@@ -159,6 +351,7 @@ function showAlert(type, messagge) {
 function getTodosFromStorage() {
     let todos;
 
+
     if (localStorage.getItem("todos") === null) {
         todos = [];
 
@@ -170,10 +363,57 @@ function getTodosFromStorage() {
 
 }
 
-function addTodoToStorage(newTodo) {
+function getDoTodosFromStorage() {
+    let doTodos;
 
+
+    if (localStorage.getItem("doTodos") === null) {
+        doTodos = [];
+
+    } else {
+        doTodos = JSON.parse(localStorage.getItem("doTodos"));
+    }
+
+    return doTodos;
+
+}
+
+function getdontTodosFromStorage() {
+    let dontTodos;
+
+
+    if (localStorage.getItem("dontTodos") === null) {
+        dontTodos = [];
+
+    } else {
+        dontTodos = JSON.parse(localStorage.getItem("dontTodos"));
+    }
+
+    return dontTodos;
+
+}
+
+function getAllTodosFromStorage() {
+    let allTodos;
+
+
+    if (localStorage.getItem("allTodos") === null) {
+        allTodos = [];
+
+    } else {
+        allTodos = JSON.parse(localStorage.getItem("allTodos"));
+    }
+
+    return allTodos;
+
+}
+
+function addTodoToStorage(newTodo, newDate) {
+    let t = [newTodo, newDate];
+    console.log(t);
     let todos = getTodosFromStorage();
-    todos.push(newTodo);
+    todos.push(t);
+    console.log(todos);
 
     localStorage.setItem("todos", JSON.stringify(todos));
 
@@ -181,6 +421,31 @@ function addTodoToStorage(newTodo) {
 
 }
 
+function addTodoDOToStorage(newTodo, newDate) {
+    let t = [newTodo, newDate];
+    console.log(t);
+    let todos = getDoTodosFromStorage();
+    todos.push(t);
+    console.log(todos);
+
+    localStorage.setItem("doTodos", JSON.stringify(todos));
+
+
+
+}
+
+function addTodoAllToStorage(newTodo, newDate) {
+    let t = [newTodo, newDate];
+    console.log(t);
+    let todos = getAllTodosFromStorage();
+    todos.push(t);
+    console.log(todos);
+
+    localStorage.setItem("allTodos", JSON.stringify(todos));
+
+
+
+}
 
 // $(function() {
 //     $("#gnd").on("click", function(e) {
